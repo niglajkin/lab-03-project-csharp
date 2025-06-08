e57895e4796f3cbad929a3d358fce55e5d5c6698
Ihlaikin Mykola <Ggg501434@gmail.com>
2025-06-08 20:29:21 +0300

    Add Show/Delete All, Clean Console options

--- Program.cs
+++ Program.cs
@@ -19,6 +19,9 @@ class Program
         }
 
         var db = mux.GetDatabase();
+        var endpoint = mux.GetEndPoints()[0];
+        var server = mux.GetServer(endpoint);
+
         AnsiConsole.MarkupLine($"[green]Connected to Redis at[/] [yellow]{redisConn}[/]");
 
         while (true)
@@ -26,7 +29,15 @@ class Program
             var action = AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
                     .Title("\nChoose an [green]operation[/]:")
-                    .AddChoices(new[] { "Add (set)", "Find (get)", "Delete", "Exit" }));
+                    .AddChoices(new[] {
+                        "Show All",
+                        "Add (set)",
+                        "Find (get)",
+                        "Delete",
+                        "Remove All",
+                        "Clear Console",
+                        "Exit"
+                    }));
 
             if (action == "Exit")
             {
@@ -34,32 +45,61 @@ class Program
                 break;
             }
 
-            var key = AnsiConsole.Ask<string>("[blue]Enter key[/]:");
-
             switch (action)
             {
+                case "Show All":
+                    var table = new Table();
+                    table.AddColumn("Key");
+                    table.AddColumn("Value");
+                    foreach (var key in server.Keys())
+                    {
+                        var val = db.StringGet(key);
+                        table.AddRow(key, val.IsNull ? "<null>" : val);
+                    }
+                    AnsiConsole.Write(table);
+                    break;
+
                 case "Add (set)":
-                    var value = AnsiConsole.Prompt(
+                    var keyToSet = AnsiConsole.Ask<string>("[blue]Enter key[/]:");
+                    var valueToSet = AnsiConsole.Prompt(
                         new TextPrompt<string>("[blue]Enter value[/]:")
                             .PromptStyle("green"));
-                    db.StringSet(key, value);
-                    AnsiConsole.MarkupLine($"[green]Saved[/] key=[yellow]{key}[/] value=[yellow]{value}[/]");
+                    db.StringSet(keyToSet, valueToSet);
+                    AnsiConsole.MarkupLine($"[green]Saved[/] key=[yellow]{keyToSet}[/] value=[yellow]{valueToSet}[/]");
                     break;
 
                 case "Find (get)":
-                    var result = db.StringGet(key);
+                    var keyToGet = AnsiConsole.Ask<string>("[blue]Enter key[/]:");
+                    var result = db.StringGet(keyToGet);
                     if (result.IsNull)
-                        AnsiConsole.MarkupLine($"[red]Key [yellow]{key}[/] not found[/]");
+                        AnsiConsole.MarkupLine($"[red]Key [yellow]{keyToGet}[/] not found[/]");
                     else
-                        AnsiConsole.MarkupLine($"[green]Value for[/] [yellow]{key}[/]: [blue]{result}[/]");
+                        AnsiConsole.MarkupLine($"[green]Value for[/] [yellow]{keyToGet}[/]: [blue]{result}[/]");
                     break;
 
                 case "Delete":
-                    var removed = db.KeyDelete(key);
+                    var keyToDelete = AnsiConsole.Ask<string>("[blue]Enter key[/]:");
+                    var removed = db.KeyDelete(keyToDelete);
                     if (removed)
-                        AnsiConsole.MarkupLine($"[green]Deleted[/] key=[yellow]{key}[/]");
+                        AnsiConsole.MarkupLine($"[green]Deleted[/] key=[yellow]{keyToDelete}[/]");
                     else
-                        AnsiConsole.MarkupLine($"[red]Key [yellow]{key}[/] not found[/]");
+                        AnsiConsole.MarkupLine($"[red]Key [yellow]{keyToDelete}[/] not found[/]");
+                    break;
+
+                case "Remove All":
+                    if (AnsiConsole.Confirm("Are you sure you want to remove *all* keys?"))
+                    {
+                        foreach (var key in server.Keys())
+                        {
+                            db.KeyDelete(key);
+                        }
+                        AnsiConsole.MarkupLine("[red]All keys deleted[/]");
+                    }
+                    break;
+
+                case "Clear Console":
+                    Console.Clear();
+                    AnsiConsole.MarkupLine($"[green]Screen cleared at {DateTime.Now:T}[/]");
                     break;
             }
         }
